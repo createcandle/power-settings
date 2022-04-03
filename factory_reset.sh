2bc647199e3718c8c177093ecc7fc8c17235d35e
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-04-03 22:45:02 +0200

    Add files via upload

--- factory_reset.sh
+++ factory_reset.sh
@@ -104,6 +104,15 @@ else
 fi
 
 
+# Try to create the hostname symbolic links
+echo "candle" > /home/pi/.webthings/etc/hostname
+echo "127.0.0.1	localhost\n::1		localhost ip6-localhost ip6-loopback\nff02::1		ip6-allnodes\nff02::2		ip6-allrouters\n\n127.0.1.1	candle" > /home/pi/.webthings/etc/hosts
+rm /etc/hostname
+rm /etc/hosts
+ln -s /home/pi/.webthings/etc/hostname /etc/hostname
+ln -s /home/pi/.webthings/etc/hosts /etc/hosts
+
+
 # Disable SSH access
 raspi-config nonint do_ssh 1 # 0 is enable, 1 is disable
 
