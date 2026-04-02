90d5e5d64923b7f15f45b12ff7721bc9c7de0f12
flatsiedatsie <unfold@gmail.com>
2026-04-02 13:28:06 +0200

    Add files via upload

--- pkg/power_settings.py
+++ pkg/power_settings.py
@@ -1018,15 +1018,18 @@ class PowerSettingsAPIHandler(APIHandler):
             pointer_output = run_command("DISPLAY=:0 xinput | grep pointer | tail -n +2 | grep -v ' XTEST '") #  | cut -f1 -d$'\t'     # | grep -v ' XTEST '
             if pointer_output != None:
                 for line in str(pointer_output).splitlines():
-                    print("pointer_output line: " + str(line))
+                    if self.DEBUG:
+                        print("pointer_output line: " + str(line))
                 
                     input_name = re.split(r'\t+', line)[0]
-                    print("input_name again: " + str(input_name))
+                    if self.DEBUG:
+                        print("input_name again: " + str(input_name))
                 
                     input_name = input_name[5:].strip()
                 
-                    print("input_name again2: " + str(input_name))
-                    print("int(rotation): " + str(int(rotation)))
+                    if self.DEBUG:
+                        print("input_name again2: " + str(input_name))
+                        print("int(rotation): " + str(int(rotation)))
                 
                     if int(rotation) == 0:
                         os.system("DISPLAY=:0 xinput --set-prop '" + str(input_name) + "' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1")
